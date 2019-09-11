a5179aa154c7295f688f2c356d92c308aafa0e05
unknown <wadeg1989@gmail.com>
2019-09-11 11:28:48 -0400

    train rl scripts now default to available cpu resource counts

--- train_dqn.sh
+++ train_dqn.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python run.py -sim double -n 7 -l 1 -tsc dqn -lr 0.00005 -lre 0.0000001 -nreplay 15000 -nsteps 2 -target_freq 128 -updates 15000 -batch 32 -save -nogui -n_hidden 3 -mode train -gmin 5
+python run.py -sim double -tsc dqn -lr 0.00005 -lre 0.0000001 -nreplay 15000 -nsteps 2 -target_freq 128 -updates 15000 -batch 32 -save -nogui -n_hidden 3 -mode train -gmin 5
